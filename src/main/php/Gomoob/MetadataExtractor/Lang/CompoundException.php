b6071ee30bac05bfd9aa0d3c741e9d0b630bca0e
Baptiste Gaillard <baptiste.gaillard@gomoob.com>
2016-08-09 09:52:21 +0200

    Continue to search Java execution problems on Travis.

--- src/main/php/Gomoob/MetadataExtractor/Lang/CompoundException.php
+++ src/main/php/Gomoob/MetadataExtractor/Lang/CompoundException.php
@@ -2,11 +2,11 @@
 
 /**
  * gomoob/php-metadata-extractor
-*
-* @copyright Copyright (c) 2016, GOMOOB SARL (http://gomoob.com)
-* @license   http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE.md file)
-*/
-namespace Gomoob\MetadataExtractor\Utils;
+ *
+ * @copyright Copyright (c) 2016, GOMOOB SARL (http://gomoob.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE.md file)
+ */
+namespace Gomoob\MetadataExtractor\Lang;
 
 /**
  * Represents a compound exception, as modelled in JDK 1.4, but unavailable in previous versions.  This class allows
---
+++ src/main/php/Gomoob/MetadataExtractor/Lang/Rational.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * gomoob/php-metadata-extractor
+ *
+ * @copyright Copyright (c) 2016, GOMOOB SARL (http://gomoob.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE.md file)
+ */
+namespace Gomoob\MetadataExtractor\Lang;
+
+/**
+ * Immutable class for holding a rational number without loss of precision. Provides a familiar representation via
+ * {@link Rational#toString} in form <code>numerator/denominator</code>.
+ *
+ * Note that any value with a numerator of zero will be treated as zero, even if the denominator is also zero.
+ *
+ * @author Baptiste GAILLARD (baptiste.gaillard@gomoob.com)
+ */
+class Rational extends \Exception
+{
+
+}
